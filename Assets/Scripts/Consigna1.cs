3eb78cf229854b58a2e755916d4bc82657e7e495
JuanWilcke <46919034@est.ort.edu.ar>
2022-04-07 14:02:39 -0300

    Initial commit

---
+++ Assets/Scripts/Consigna1.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consigna1 : MonoBehaviour
+//1.Crea un programa que permita ingresar por Inspector dos valores enteros
+//y responda si son iguales o no lo son.
+{
+    public int num1;
+    public int num2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1==num2)
+        {
+            Debug.Log("Los números ingresados son iguales");
+        }
+        else
+        {
+            Debug.Log("Los numeros ingresados no son iguales");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Consigna2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consigna2 : MonoBehaviour
+    //2. Crear las variables necesarias para ingresar 
+    //por Inspector el precio de 3 productos y un monto de dinero disponible.
+    //Comunicar si la suma de los mismos supera o no el monto de dinero disponible.
+    //Luego indicar cuánto dinero sobra o falta.
+{
+    public float precio1;
+    public float precio2;
+    public float precio3;
+    public float dinero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        float totalprecio = precio1 + precio2 + precio3;
+        float Resto = totalprecio - dinero;
+        if (totalprecio>dinero)
+        {
+            Debug.Log("El total a pagar es mayor al monto disponible");
+        }
+        if (totalprecio<=dinero)
+        {
+            Debug.Log("El monto es suficiente para pagar el total");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
